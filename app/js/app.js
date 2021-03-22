c72d39701b04885729982b39a289d42bf5b0454e
Zhemchuzhnov Dmitriy <zhemchuzhnov.d@gmail.com>
2021-03-22 14:44:17 +0300

    Финальная версия хедера

--- app/js/app.js
+++ app/js/app.js
@@ -1,7 +1,7 @@
 // // Import jQuery module (npm i jquery)
-// import $ from 'jquery'
-// window.jQuery = $
-// window.$ = $
+import $ from 'jquery';
+window.jQuery = $;
+window.$ = $;
 
 // // Import vendor jQuery plugin example (not module)
 // require('~/app/libs/mmenu/dist/mmenu.js')
@@ -9,5 +9,8 @@
 document.addEventListener('DOMContentLoaded', () => {
 
 	// Custom JS
+    $('.menu-btn').on('click', () => {
+        $('.navigation').toggleClass('show');
+    });
 
-})
+});
