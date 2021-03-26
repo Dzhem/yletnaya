582e2495cb961a3ab014fe9ecf5db62c6663c328
Zhemchuzhnov Dmitriy <zhemchuzhnov.d@gmail.com>
2021-03-26 10:49:24 +0300

    Страница Контакты

--- app/js/app.js
+++ app/js/app.js
@@ -8,9 +8,28 @@ window.$ = $;
 
 document.addEventListener('DOMContentLoaded', () => {
 
-	// Custom JS
+    // Custom JS
     $('.menu-btn').on('click', () => {
         $('.navigation').toggleClass('show');
     });
+
+    genBreadcrumb();
+
+    function genBreadcrumb() {
+        const pageName = document.location.pathname;
+        const breadcrumbActive = document.querySelector('.breadcrumb>.active');
+        const pages = {
+            '/dostavka-i-oplata.html': 'Доставка и Оплата',
+            '/kontaktyi.html': 'Контакты',
+            '/skidki-i-akczii.html': 'Скидки'
+        }
+
+        for (let page in pages) {
+            if (page === pageName) {
+                breadcrumbActive.textContent = pages[page];
+            }
+        }
+    }
+
 
 });
